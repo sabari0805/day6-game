eddd1476c210d3afc3911a9314fc461e0d75dd26
sabari0805 <sabari0158@gmail.com>
2024-07-10 07:48:44 -0700

    Add files via upload

---
+++ design.js
@@ -0,0 +1,22 @@
+var num=Math.floor(Math.random()*99);
+var count=0;
+console.log(num);
+
+function findnum()
+{
+    var guessed_num=parseInt(document.getElementById("n1").value);
+    count+=1;
+    if(guessed_num==num)
+    {
+        document.getElementById("result").textContent="Congratulations....";
+    }
+    else if(guessed_num>num)
+    {     
+        document.getElementById("result").textContent="Try with smaller number";
+    }
+    else{
+        document.getElementById("result").textContent="Try with greater number";
+    }
+    document.getElementById("count").textContent=count;
+
+}
